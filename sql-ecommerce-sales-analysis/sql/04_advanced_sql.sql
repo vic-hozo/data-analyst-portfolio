7f2f705a330b88b30ea0af4555dcb9d6cfefe5f8
vic-hozo <pinvichozo@gmail.com>
2026-07-25 21:11:36 +0800

    Update 04_advanced_sql.sql

--- sql-ecommerce-sales-analysis/sql/04_advanced_sql.sql
+++ sql-ecommerce-sales-analysis/sql/04_advanced_sql.sql
@@ -22,4 +22,40 @@ FROM (
 	GROUP BY customer_name
 	) as customer_total;
 	
-	
+-- Windows Function. RANK(), PARTITION BY, subquery
+-- Top Customer in Each Region
+-- Purpose: Identify the highest-spending customer in every region.
+
+SELECT *
+FROM
+(
+    -- Rank customers within each region based on their total sales
+    SELECT
+        customer_name,
+        region,
+        ROUND(total_sales, 2) as total_sales,
+        -- Assign a rank to each customer within their region.
+        -- Ranking starts again at 1 for each region.
+        RANK() OVER (
+            PARTITION BY region
+            ORDER BY total_sales DESC
+        ) as sales_rank
+
+    FROM
+    (
+        -- Calculate the total sales for each customer in each region
+        SELECT
+            customer_name,
+            region,
+            SUM(sales) as total_sales
+        FROM orders
+        -- Group the sales by customer and region
+        GROUP BY customer_name, region
+
+    ) as customer_sales
+
+) as overall
+-- Only display customers ranked 1st, 2nd, or 3rd
+WHERE sales_rank <= 3;
+
+
